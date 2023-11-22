9adbf6e33e2545455cba849e88f207d54323792b
YewonCALLI <96384200+YewonCALLI@users.noreply.github.com>
2023-11-22 21:28:51 +0900

    babel 오류 해결

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, ScrollControls, useScroll } from '@react-three/drei'
 import { getProject, val } from '@theatre/core'
-import { SheetProvider, PerspectiveCamera, useCurrentSheet } from '@theatre/r3f'
 
 import {Modal} from './Modal.js'
 import MyElement3D from './MyElement3D.js'
@@ -31,15 +30,8 @@ function App() {
       camera = {{position: [0, 2, 0], fov: 90}}
       >
       <MyElement3D />
-      {/* <JisuModel/> */}
       <OrbitControls/>
-      {/* <ScrollControls pages={5} damping={1} maxSpeed={0.1}>
-          <SheetProvider sheet={sheet}>
-            <Scene />
-          </SheetProvider>
-        </ScrollControls> */}
-     </Canvas>
-      {/* <Modal/> */}
+      </Canvas>
       <a className="click-button" 
       onClick={() => {
         openModal();
@@ -52,31 +44,5 @@ function App() {
 
 export default App;
 
-function Scene() {
-  const sheet = useCurrentSheet()
-  const scroll = useScroll()
 
-  // our callback will run on every animation frame
-  useFrame(() => {
-    // the length of our sequence
-    const sequenceLength = val(sheet.sequence.pointer.length)
-    // update the "position" of the playhead in the sequence, as a fraction of its whole length
-    sheet.sequence.position = scroll.offset * sequenceLength
-  })
-
-  return (
-    <>
-      <ambientLight />
-      <MyElement3D/>
-      <PerspectiveCamera
-        theatreKey='Camera'
-        makeDefault
-        position={[0, 0, 0]}
-        fov={180}
-        near={0.1}
-        far={70}
-      />
-    </>
-  )
-}
 
